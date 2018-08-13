c2f6570553a638d053c8c05195770d42acec07cf
Marifel Barbasa <mrbarbasa@gmail.com>
2018-08-13 01:32:09 -1000

    Consolidate the GloVe and fastText embeddings loader function into one

--- code/packages/__init__.py
+++ code/packages/__init__.py
@@ -25,8 +25,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 
 from preprocessing import compute_word_index, construct_embedding_matrix
-from preprocessing import load_glove_embeddings, load_fasttext_embeddings
-from preprocessing import integer_encode_classes, one_hot_encode_classes
+from preprocessing import load_embeddings, integer_encode_classes, one_hot_encode_classes
 from stats import display_classification_summary, save_classification_summary
 from utils import load_data, save_line_to_file, format_time_str, get_time_elapsed
 from validation import calculate_logloss
--- code/preprocessing/__init__.py
+++ code/preprocessing/__init__.py
@@ -43,29 +43,16 @@ def compute_word_index(X_train_sequences,
     print('Found {} unique tokens.'.format(len(word_index)))
     return X_train_tokenized, X_test_tokenized, word_index
 
-def load_glove_embeddings(embeddings_path):
-    # Load the GloVe embeddings into a dictionary
-    # This maps words (as strings) to their vector representation (as float vectors)
-    embeddings_index = {}
-    f = open(embeddings_path, encoding='utf-8')
-    for line in tqdm(f):
-        values = line.split()
-        word = values[0]
-        coefs = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = coefs
-    f.close()
-    print('Found {} word vectors.'.format(len(embeddings_index)))
-    return embeddings_index
-
 # The first line of a fastText embeddings file contains:
 # - The number of words in the vocabulary
 # - The size of each vector
 #
-# Each line contains a string word followed by its vector.
-# Each value is space-separated.
-# Words are ordered by descending frequency.
-def load_fasttext_embeddings(embeddings_path):
-    # Load the fastText embeddings into a dictionary
+# Otherwise, the rest of a fastText file has the same format as GloVe:
+# - Each line contains a string word followed by its vector.
+# - Each value is space-separated.
+# - Words are ordered by descending frequency.
+def load_embeddings(embeddings_path):
+    # Load the GloVe or fastText embeddings into a dictionary
     # This maps words (as strings) to their vector representation (as float vectors)
     embeddings_index = {}
     f = open(embeddings_path, encoding='utf-8')
