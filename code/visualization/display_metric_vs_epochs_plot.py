38ca2207a444af10a2196550ada861c7f3334139
Marifel Barbasa <mrbarbasa@gmail.com>
2018-08-27 22:16:18 -1000

    Document the functions in the visualization module

--- code/visualization/display_metric_vs_epochs_plot.py
+++ code/visualization/display_metric_vs_epochs_plot.py
@@ -1,6 +1,29 @@
 import matplotlib.pyplot as plt
 
 def display_metric_vs_epochs_plot(scores, metric, nth_iter, nth_fold):
+    """Display a metric vs. epochs plot.
+
+    Both the training and validation scores will be plotted for the
+    chosen metric.
+
+    Parameters
+    ----------
+    scores : pandas.DataFrame
+        Scores containing the `epoch` column and metric columns such as
+        `acc`, `loss`, `val_acc`, and `val_loss`.
+    metric : string
+        The metric to display, such as 'loss' or 'acc' (note that `val_`
+        is also appended automatically and should not be provided).
+    nth_iter : int
+        The current random search iteration.
+    nth_fold : int
+        The current k-fold fold.
+
+    Returns
+    -------
+    None
+    """
+
     metric_fold_scores = scores[metric].values.tolist()
     epochs = range(1, len(metric_fold_scores) + 1)
     plt.figure(figsize=(6,4))
